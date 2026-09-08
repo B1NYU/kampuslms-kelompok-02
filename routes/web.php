0e649c763ff98b5953dcaa1a11292f431988f785
clara-uenike <10241018@student.itk.ac.id>
2026-09-08 13:33:05 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::prefix('mata-kuliah')->name('mata-kuliah.')->group(function () {
     Route::get('/', [CourseController::class, 'index'])->name('index');
     Route::get('/mata_kuliah', [CourseController::class, 'show'])->name('show');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
